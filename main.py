fad4dee157d51e18c5eed4bb5d85c47936ce95fd
325197077 <rontalshaked@gmail.com>
2025-01-27 14:24:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+image_path = '007.png'
+image_array = load_image(image_path)
+clean_image = median(image_array, ball(3))
+edgeMAG = edge_detection(clean_image)
+to_print= Image.fromarray((edgeMAG * 255).astype(np.uint8))
+to_print.save('my_edges.png')
